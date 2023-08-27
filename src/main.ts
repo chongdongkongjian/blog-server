b2c583f7be3f1f4e23679a4e7c0ed7db17494ef7
虫洞空间 <15506604131@163.com>
2023-08-27 20:36:21 +0800

    feat:支持静态资源访问

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+const port = 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '../public'), { prefix: '/static' });
+  console.error('port', port);
+  await app.listen(port);
 }
 bootstrap();
