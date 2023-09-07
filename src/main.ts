bbbabd750d029f70da4bbedd7dd71356fb33272b
虫洞空间 <15506604131@163.com>
2023-09-08 00:22:22 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const port = 3000;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '../public'), { prefix: '/static' });
-  console.error('port', port);
   await app.listen(port);
 }
 bootstrap();
